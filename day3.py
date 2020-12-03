ce7d4aaef8503ec4a1ca93c3f2b42b875e23b39c
Rohan Gupta <rohangupta883@gmail.com>
2020-12-03 21:51:23 +0530

    day 3

---
+++ day3.py
@@ -0,0 +1,20 @@
+import math
+
+f = open("inputs/day3.txt", "r")
+inp = list(f.read().split("\n"))
+c = 0
+# part 1
+for i in range(1, len(inp)):
+    if inp[i][3 * i % len(inp[i])] == "#":
+        c += 1
+
+print(f"Part 1: {c}")
+# part 2
+c = [0] * 5
+for i in range(1, len(inp)):
+    c = [sum(x) for x in zip([(inp[i][(k * i) % len(inp[i])] == "#") for k in [1, 3, 5, 7]] + [0], c)]
+    if i % 2 == 0:
+        if inp[i][(i // 2) % len(inp[i])] == "#":
+            c[4] += 1
+
+print(f"Part 2: {math.prod(c)}")
