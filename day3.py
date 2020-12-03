b00495b1b96ca9ecb25840efe45a75e69c34813a
Rohan Gupta <rohangupta883@gmail.com>
2020-12-03 22:05:11 +0530

    cleaned 3

--- day3.py
+++ day3.py
@@ -2,14 +2,7 @@ import math
 
 f = open("inputs/day3.txt", "r")
 inp = list(f.read().split("\n"))
-c = 0
-# part 1
-for i in range(1, len(inp)):
-    if inp[i][3 * i % len(inp[i])] == "#":
-        c += 1
 
-print(f"Part 1: {c}")
-# part 2
 c = [0] * 5
 for i in range(1, len(inp)):
     c = [sum(x) for x in zip([(inp[i][(k * i) % len(inp[i])] == "#") for k in [1, 3, 5, 7]] + [0], c)]
@@ -17,4 +10,5 @@ for i in range(1, len(inp)):
         if inp[i][(i // 2) % len(inp[i])] == "#":
             c[4] += 1
 
+print(f"Part 1: {c[1]}")
 print(f"Part 2: {math.prod(c)}")
